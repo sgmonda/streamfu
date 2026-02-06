f665e1c4562654f7c3f37db0215014d0bc608949
Sergio García Mondaray <sgmonda@gmail.com>
2026-02-07 00:37:52 +0100

    test: better branch() tests

--- src/branch.test.ts
+++ src/branch.test.ts
@@ -8,23 +8,31 @@ const TEST_CASES = [
     title: "empty stream",
     iterable: [],
     sum: 0,
+    mul: 1,
   },
   {
     title: "one item",
     iterable: [1],
     sum: 1,
+    mul: 1,
   },
   {
     title: "many items",
     iterable: [1, 2, 6, 1],
     sum: 10,
+    mul: 12,
   },
 ]
 
 Deno.test("branch()", async ({ step }) => {
   for (const testCase of TEST_CASES) await runTest(testCase)
 
-  async function runTest({ title, iterable, sum }: (typeof TEST_CASES)[0]) {
+  async function runTest({
+    title,
+    iterable,
+    sum,
+    mul,
+  }: (typeof TEST_CASES)[0]) {
     await step(title, async () => {
       const stream = createReadable(iterable)
       assertEquals(stream.locked, false)
@@ -32,11 +40,14 @@ Deno.test("branch()", async ({ step }) => {
       const branches = branch(stream, 5)
       assertEquals(branches.length, 5)
 
-      const promises = branches.map(async (b) => {
-        const observed = await reduce(b, (acc, v) => acc + v, 0)
-        assertEquals(observed, sum)
-      })
-      await Promise.all(promises)
+      assertEquals(await reduce(branches[0], sumReducer, 0), sum)
+      assertEquals(await reduce(branches[1], sumReducer, 0), sum)
+      assertEquals(await reduce(branches[2], mulReducer, 1), mul)
+      assertEquals(await reduce(branches[3], mulReducer, 1), mul)
+      assertEquals(await reduce(branches[4], mulReducer, 1), mul)
     })
   }
 })
+
+const sumReducer = (acc: number, v: number) => acc + v
+const mulReducer = (acc: number, v: number) => acc * v
